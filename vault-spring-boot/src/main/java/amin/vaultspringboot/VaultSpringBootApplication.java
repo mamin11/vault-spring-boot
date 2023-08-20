d5529028860549a75942de4819cf7a81d0a9d193
Amin <abdimamin@gmail.com>
2023-08-20 12:27:43 +0100

    initial commit

---
+++ vault-spring-boot/src/main/java/amin/vaultspringboot/VaultSpringBootApplication.java
@@ -0,0 +1,13 @@
+package amin.vaultspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultSpringBootApplication.class, args);
+	}
+
+}
---
+++ vault-spring-boot/src/test/java/amin/vaultspringboot/VaultSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package amin.vaultspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
